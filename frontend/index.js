301979ba6a46f907ebdfe2c6e5bb026c48688331
ACAPP-dev <acapp909@gmail.com>
2020-05-18 15:44:11 -0700

    basic index page setup

---
+++ frontend/index.js
@@ -0,0 +1,7 @@
+const container = document.getElementById('container')
+
+const greeting = document.createElement('h2')
+greeting.innerText = 'Hello Slot Player!'
+
+container.appendChild(greeting)
+console.log("Hello Slot Player!")
