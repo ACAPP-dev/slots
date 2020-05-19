58751ee69b042c6d2e15e81e6bd51fa246c654ff
ACAPP-dev <acapp909@gmail.com>
2020-05-18 17:07:59 -0700

    image manipulation

--- frontend/index.js
+++ frontend/index.js
@@ -1,7 +1,44 @@
 const container = document.getElementById('container')
+const btnSpin = document.getElementById('spin')
+const reel1 = document.getElementById('reel-1')
+const reel1Img = reel1.querySelector('img')
+
+const slotSeven = document.createElement('img')
+
+slotSeven.id = 'seven'
+slotSeven.src = 'images/seven.png'
+console.log(slotSeven)
+
+const slotBar = document.createElement('img')
+
+slotBar.id = 'bar'
+slotBar.src = 'images/bar.png'
+console.log(slotBar)
 
 const greeting = document.createElement('h2')
 greeting.innerText = 'Hello Slot Player!'
 
 container.appendChild(greeting)
 console.log("Hello Slot Player!")
+
+btnSpin.addEventListener('click', spin)
+
+function spin() {
+    console.log('Spin!!')
+    for (let i=0; i<3; i++) {
+    selectImg1()
+    selectImg2()
+
+    }
+}
+
+function selectImg1() {
+    console.log(reel1Img)
+    setTimeout(() => {reel1Img.src = 'images/bar.png'}, 1000)
+    
+   
+}
+
+function selectImg2() {
+    setTimeout(() => {reel1Img.src = 'images/seven.png'}, 1000)
+}
