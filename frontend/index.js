74676a34002cac8e0ee8646a32136578d7a2aa5d
ACAPP-dev <acapp909@gmail.com>
2020-05-30 15:36:27 -0700

    adjust spin animations

--- frontend/index.js
+++ frontend/index.js
@@ -10,8 +10,6 @@ let reel3Img = reel3.querySelector('img')
 
 const playerMessageDiv = document.getElementById('messages')
 
-// const slotSeven = document.createElement('img')
-
 // Define image object (each reel image will be an instance)
 
 class ReelImage {
@@ -26,12 +24,7 @@ class ReelImage {
         return img
     }
 
-    move() {
-        if (this.position < 3) {
-            this.position += 1
-        }
-        else {this.position = 1}
-    }
+    
 }
 
 const imgSeven = new ReelImage('images/seven.png', 1)
@@ -44,30 +37,19 @@ reel2.appendChild(imgBar.element)
 
 reel3.appendChild(imgSeven.element)
 
-// slotSeven.id = 'seven'
-// slotSeven.src = 'images/seven.png'
-// console.log(slotSeven)
-
-// const slotBar = document.createElement('img')
-
-// slotBar.id = 'bar'
-// slotBar.src = 'images/bar.png'
-// console.log(slotBar)
-
 const greeting = document.createElement('p')
 greeting.classList.add('message-text')
 greeting.innerText = 'Hello Slot Player!'
 playerMessageDiv.appendChild(greeting)
-
 
 btnSpin.addEventListener('click', spin)
 
 function spin() {
     console.log('Spin Button Pressed!!')
     spinInterval(()=>{
-        setTimeout(()=>selectImg1(), 1100)
-        setTimeout(()=>selectImg2(), 1100)
-    }, 2300, 5)   
+        setTimeout(()=>selectImg1(), 0)
+        setTimeout(()=>selectImg2(), 500)
+    }, 1000, 5)   
 }
 
 function spinInterval(callback, delay, repetitions) {
@@ -85,38 +67,38 @@ function selectImg1() {
     reel1Img = reel1.querySelector('img')
     reel1.removeChild(reel1Img)
     reel1Img = imgBar.element
-    reel1Img.classList.add('img-spin')
+    // reel1Img.classList.add('img-spin')
     reel1.appendChild(reel1Img)  
 
-    reel2Img = reel2.querySelector('img')
-    reel2.removeChild(reel2Img)
-    reel2Img = imgBar.element
-    reel2Img.classList.add('img-spin')
-    reel2.appendChild(reel2Img)  
+    // reel2Img = reel2.querySelector('img')
+    // reel2.removeChild(reel2Img)
+    // reel2Img = imgBar.element
+    // reel2Img.classList.add('img-spin')
+    // reel2.appendChild(reel2Img)  
 
-    reel3Img = reel3.querySelector('img')
-    reel3.removeChild(reel3Img)
-    reel3Img = imgBar.element
-    reel3Img.classList.add('img-spin')
-    reel3.appendChild(reel3Img)  
+    // reel3Img = reel3.querySelector('img')
+    // reel3.removeChild(reel3Img)
+    // reel3Img = imgBar.element
+    // reel3Img.classList.add('img-spin')
+    // reel3.appendChild(reel3Img)  
 }
 
 function selectImg2() {
     reel1Img = reel1.querySelector('img')
     reel1.removeChild(reel1Img)
     reel1Img = imgSeven.element
-    reel1Img.classList.add('img-spin')
+    // reel1Img.classList.add('img-spin')
     reel1.appendChild(reel1Img)
 
-    reel2Img = reel2.querySelector('img')
-    reel2.removeChild(reel2Img)
-    reel2Img = imgSeven.element
-    reel2Img.classList.add('img-spin')
-    reel2.appendChild(reel2Img)  
+    // reel2Img = reel2.querySelector('img')
+    // reel2.removeChild(reel2Img)
+    // reel2Img = imgSeven.element
+    // reel2Img.classList.add('img-spin')
+    // reel2.appendChild(reel2Img)  
 
-    reel3Img = reel3.querySelector('img')
-    reel3.removeChild(reel3Img)
-    reel3Img = imgSeven.element
-    reel3Img.classList.add('img-spin')
-    reel3.appendChild(reel3Img)  
+    // reel3Img = reel3.querySelector('img')
+    // reel3.removeChild(reel3Img)
+    // reel3Img = imgSeven.element
+    // reel3Img.classList.add('img-spin')
+    // reel3.appendChild(reel3Img)  
 }
