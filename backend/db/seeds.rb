33b609c189578dc6dbd1f4b7b96d628154fc3e97
ACAPP-dev <acapp909@gmail.com>
2020-05-30 15:57:32 -0700

    create migration and seeds for images

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create Image Instances
+
+IMAGES = [
+    {
+        name: 'bag',
+        source: 'bag.png'
+    },
+    {
+        name: 'banana',
+        source: 'banana.jpg'
+    },
+    {
+        name: 'bar',
+        source: 'bar.png'
+    },
+    {
+        name: 'cherry',
+        source: 'cherry.jpg'
+    },
+    {
+        name: 'dollar',
+        source: 'dollar.png'
+    },
+    {
+        name: 'gold-seven',
+        source: 'gold-seven.png'
+    },
+    {
+        name: 'seven',
+        source: 'seven.png'
+    },
+    {
+        name: 'triple-seven',
+        source: 'triple-seven.png'
+    },
+    {
+        name: 'watermelon',
+        source: 'watermelon.png'
+    }
+]
+
+IMAGES.each do |image|
+    Image.create(name: image[:name], source: image[:source])
+end
+
