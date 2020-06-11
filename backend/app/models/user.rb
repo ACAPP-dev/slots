16c2114a5c39315b7355e3fec4d91e19d91ff252
ACAPP-dev <acapp909@gmail.com>
2020-06-11 15:55:34 -0700

    bug fixes - validations"

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_many :transactions
     has_secure_password
 
+    validates :name, :username, presence: true
+
     def self.last_5_transactions(user)
         user.transactions.last(5)
     end
